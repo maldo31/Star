b2bf516988bbb558014119f88118c2e5de0ce6c5
maldo <maldok31@gmail.com>
2021-12-09 13:55:37 +0100

    first

---
+++ app/src/main/java/com/example/star/Register.java
@@ -0,0 +1,34 @@
+package com.example.star;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    EditText mUsername,mEmail,mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mUsername = findViewById(R.id.editUsername);
+        mEmail = findViewById(R.id.editTextTextEmailAddress);
+        mPassword = findViewById(R.id.editTextTextPassword2);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.createText);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+    }
+}
